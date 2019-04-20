41b523d7ccf83fa047f63fa67b2f1c61e19a6b0d
Deepak M. Kathayat <dmkathayat@gmail.com>
2019-04-20 18:21:04 +0530

    remove junk

--- projects/hello_cargo/target/debug/deps/hello_cargo-b7df8cc17f162a33.d
+++
@@ -1,5 +0,0 @@
-/home/dmkathayat/exercism/rust/projects/hello_cargo/target/debug/deps/hello_cargo-b7df8cc17f162a33: src/main.rs
-
-/home/dmkathayat/exercism/rust/projects/hello_cargo/target/debug/deps/hello_cargo-b7df8cc17f162a33.d: src/main.rs
-
-src/main.rs:
--- projects/hello_cargo/target/debug/hello_cargo.d
+++
@@ -1,1 +0,0 @@
-/home/dmkathayat/exercism/rust/projects/hello_cargo/target/debug/hello_cargo: /home/dmkathayat/exercism/rust/projects/hello_cargo/src/main.rs
