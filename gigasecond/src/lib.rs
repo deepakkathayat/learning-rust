69bd35aa79657cd1ebfbe221f5b556efc9d7b82b
Deepak M. Kathayat <dmkathayat@gmail.com>
2019-04-20 13:02:41 +0530

    check for overflow

--- gigasecond/src/lib.rs
+++ gigasecond/src/lib.rs
@@ -4,5 +4,7 @@ const BILLION: i64 = 1_000_000_000;
 
 // Returns a Utc DateTime one billion seconds after start.
 pub fn after(start: DateTime<Utc>) -> DateTime<Utc> {
-    start + Duration::seconds(BILLION)
+    start
+        .checked_add_signed(Duration::seconds(BILLION))
+        .expect("Overflow!")
 }
