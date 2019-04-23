60e7ef3c99d5b21688b42da0aabd26d008ee407b
Deepak M. Kathayat <dmkathayat@gmail.com>
2019-04-23 08:47:36 +0530

    clean up

--- beer-song/src/lib.rs
+++ beer-song/src/lib.rs
@@ -1,29 +1,30 @@
 fn bottles(n: i32) -> String {
     match n {
         1 => "1 bottle of beer".to_string(),
-        0 => "No more bottles of beer".to_string(),
+        0 => "no more bottles of beer".to_string(),
         -1 => "99 bottles of beer".to_string(),
         _ => n.to_string() + " bottles of beer",
     }
 }
 
-fn phrase(n: i32) -> String {
+fn first_phrase(n: i32) -> String {
     match n {
-        1 => "Take it down and pass it around,".to_string(),
-        0 => "Go to the store and buy some more,".to_string(),
-        _ => "Take one down and pass it around,".to_string(),
+        1 => "1 bottle of beer on the wall, 1 bottle of beer.".to_string(),
+        0 => "No more bottles of beer on the wall, no more bottles of beer.".to_string(),
+        _ => bottles(n) + " on the wall, " + &bottles(n) + ".",
+    }
+}
+
+fn second_phrase(n: i32) -> String {
+    match n {
+        1 => "Take it down and pass it around, ".to_string() + &bottles(n - 1) + " on the wall.",
+        0 => "Go to the store and buy some more, ".to_string() + &bottles(n - 1) + " on the wall.",
+        _ => "Take one down and pass it around, ".to_string() + &bottles(n - 1) + " on the wall.",
     }
 }
 
 pub fn verse(n: i32) -> String {
-    let n_bottles = bottles(n);
-    let first_sentence = n_bottles.clone() + " on the wall, " + &n_bottles + ".";
-
-    let n_phrase = phrase(n);
-    let nminus_bottles = bottles(n - 1);
-    let second_sentence = n_phrase.to_string() + " " + &nminus_bottles + " on the wall.";
-
-    first_sentence + "\n" + &second_sentence + "\n"
+    first_phrase(n) + "\n" + &second_phrase(n) + "\n"
 }
 
 pub fn sing(start: i32, end: i32) -> String {
