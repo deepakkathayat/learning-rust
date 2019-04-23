6585656a6986f6bd43181cba14344b61ef9af574
Deepak M. Kathayat <dmkathayat@gmail.com>
2019-04-23 22:19:32 +0530

    implement proverb

--- proverb/src/lib.rs
+++ proverb/src/lib.rs
@@ -1,3 +1,31 @@
 pub fn build_proverb(list: &[&str]) -> String {
-    unimplemented!("build a proverb from this list of items: {:?}", list)
+    let mut result = String::new();
+
+    let mut list_iter = list.iter();
+
+    let mut first_word;
+
+    match list_iter.next() {
+        Some(v) => first_word = v.to_string(),
+        None => return result,
+    }
+
+    let mut word = first_word.clone();
+    loop {
+        match list_iter.next() {
+            Some(x) => {
+                result = result + &format!("For want of a {} the {} was lost.\n", word, x);
+                word = x.to_string();
+            }
+            None => {
+                if first_word.is_empty() {
+                    break;
+                } else {
+                    result = result + &format!("And all for the want of a {}.", first_word);
+                    break;
+                }
+            }
+        }
+    }
+    result
 }
