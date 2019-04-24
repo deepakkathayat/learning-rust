ab85f5a393c01546ffd85944c69e3660c3cce28a
Deepak M. Kathayat <dmkathayat@gmail.com>
2019-04-24 22:03:29 +0530

    use formula

--- difference-of-squares/src/lib.rs
+++ difference-of-squares/src/lib.rs
@@ -1,19 +1,9 @@
 pub fn square_of_sum(n: u32) -> u32 {
-    let mut result = 0;
-
-    for i in 1..n + 1 {
-        result += i;
-    }
-    result * result
+    ((n * (n + 1)) / 2).pow(2)
 }
 
 pub fn sum_of_squares(n: u32) -> u32 {
-    let mut result = 0;
-
-    for i in 1..n + 1 {
-        result += i * i;
-    }
-    result
+    (n * (n + 1) * (2 * n + 1)) / 6
 }
 
 pub fn difference(n: u32) -> u32 {
