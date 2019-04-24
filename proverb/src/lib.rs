c83ed00977a68f192b55cb3246513210114a4a3d
Deepak M. Kathayat <dmkathayat@gmail.com>
2019-04-24 21:37:56 +0530

    try a new way

--- proverb/src/lib.rs
+++ proverb/src/lib.rs
@@ -1,31 +1,17 @@
+use std::iter;
+
 pub fn build_proverb(list: &[&str]) -> String {
-    let mut result = String::new();
+    let list_vec: Vec<_> = list.to_vec();
 
-    let mut list_iter = list.iter();
-
-    let mut first_word;
-
-    match list_iter.next() {
-        Some(v) => first_word = v.to_string(),
-        None => return result,
+    if list_vec.is_empty() {
+        String::new()
+    } else {
+        let last_line = format!("And all for the want of a {}.", list_vec[0]);
+        list_vec
+            .windows(2)
+            .map(|win| format!("For want of a {} the {} was lost.", win[0], win[1]))
+            .chain(iter::once(last_line))
+            .collect::<Vec<_>>()
+            .join("\n")
     }
-
-    let mut word = first_word.clone();
-    loop {
-        match list_iter.next() {
-            Some(x) => {
-                result = result + &format!("For want of a {} the {} was lost.\n", word, x);
-                word = x.to_string();
-            }
-            None => {
-                if first_word.is_empty() {
-                    break;
-                } else {
-                    result = result + &format!("And all for the want of a {}.", first_word);
-                    break;
-                }
-            }
-        }
-    }
-    result
 }
