db8d9fbe258b9d92a2017ff2e5e621a4208c31d6
Deepak M. Kathayat <dmkathayat@gmail.com>
2019-04-25 08:07:27 +0530

    remove duplicates

--- sum-of-multiples/src/lib.rs
+++ sum-of-multiples/src/lib.rs
@@ -1,15 +1,21 @@
-fn multiples(limit: u32, x: u32) -> u32 {
+use itertools::Itertools;
+
+fn multiples(limit: u32, x: u32) -> Vec<u32> {
     if limit < x || x == 0 {
-        0
+        vec![0]
     } else if limit % x == 0 {
-        (1..limit / x).map(|i| i * x).sum()
+        (1..limit / x).map(|i| i * x).collect::<Vec<u32>>()
     } else {
-        (1..(limit / x) + 1).map(|i| i * x).sum()
+        (1..(limit / x) + 1).map(|i| i * x).collect::<Vec<u32>>()
     }
 }
 
 pub fn sum_of_multiples(limit: u32, factors: &[u32]) -> u32 {
     let list = factors.to_vec();
 
-    list.into_iter().map(|x| multiples(limit, x)).sum()
+    list.into_iter()
+        .map(|x| multiples(limit, x))
+        .flatten()
+        .unique()
+        .sum()
 }
