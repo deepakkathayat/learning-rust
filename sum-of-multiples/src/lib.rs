642e9ee68ff421617770382fe455010554c9f826
Deepak M. Kathayat <dmkathayat@gmail.com>
2019-04-25 07:47:45 +0530

    handling edge cases

--- sum-of-multiples/src/lib.rs
+++ sum-of-multiples/src/lib.rs
@@ -1,9 +1,15 @@
-fn multiples(x: u32, limit: u32) -> u32 {
-    (1..limit / x).map(|i| i * x).filter(|n| n < &limit).sum()
+fn multiples(limit: u32, x: u32) -> u32 {
+    if limit < x || x == 0 {
+        0
+    } else if limit % x == 0 {
+        (1..limit / x).map(|i| i * x).sum()
+    } else {
+        (1..(limit / x) + 1).map(|i| i * x).sum()
+    }
 }
 
 pub fn sum_of_multiples(limit: u32, factors: &[u32]) -> u32 {
     let list = factors.to_vec();
 
-    list.into_iter().map(|x| multiples(x, limit)).sum()
+    list.into_iter().map(|x| multiples(limit, x)).sum()
 }
