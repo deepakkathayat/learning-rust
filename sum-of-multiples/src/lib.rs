fb12b5de1341f5d2052d35decd1b391870c37ddc
Deepak M. Kathayat <dmkathayat@gmail.com>
2019-04-25 07:36:46 +0530

    use map and filter

--- sum-of-multiples/src/lib.rs
+++ sum-of-multiples/src/lib.rs
@@ -1,7 +1,9 @@
+fn multiples(x: u32, limit: u32) -> u32 {
+    (1..limit / x).map(|i| i * x).filter(|n| n < &limit).sum()
+}
+
 pub fn sum_of_multiples(limit: u32, factors: &[u32]) -> u32 {
-    unimplemented!(
-        "Sum the multiples of all of {:?} which are less than {}",
-        factors,
-        limit
-    )
+    let list = factors.to_vec();
+
+    list.into_iter().map(|x| multiples(x, limit)).sum()
 }
