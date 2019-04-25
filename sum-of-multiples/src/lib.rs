30bb0f697fa4cea0fc5ed605d1d35ea8a7c7b564
Deepak M. Kathayat <dmkathayat@gmail.com>
2019-04-25 08:18:22 +0530

    one liner

--- sum-of-multiples/src/lib.rs
+++ sum-of-multiples/src/lib.rs
@@ -1,21 +1,5 @@
-use itertools::Itertools;
-
-fn multiples(limit: u32, x: u32) -> Vec<u32> {
-    if limit < x || x == 0 {
-        vec![0]
-    } else if limit % x == 0 {
-        (1..limit / x).map(|i| i * x).collect::<Vec<u32>>()
-    } else {
-        (1..(limit / x) + 1).map(|i| i * x).collect::<Vec<u32>>()
-    }
-}
-
 pub fn sum_of_multiples(limit: u32, factors: &[u32]) -> u32 {
-    let list = factors.to_vec();
-
-    list.into_iter()
-        .map(|x| multiples(limit, x))
-        .flatten()
-        .unique()
+    (1..limit)
+        .filter(|n| factors.iter().any(|factor| *factor != 0 && n % factor == 0))
         .sum()
 }
