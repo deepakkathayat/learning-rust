47262b51d8f945140af20be41f2cb535cd6e54b7
Deepak M. Kathayat <dmkathayat@gmail.com>
2019-04-27 17:34:34 +0530

    using loop

--- prime-factors/src/lib.rs
+++ prime-factors/src/lib.rs
@@ -1,3 +1,21 @@
-pub fn factors(n: u64) -> Vec<u64> {
-    unimplemented!("This should calculate the prime factors of {}", n)
+pub fn factors(m: u64) -> Vec<u64> {
+    if m <= 1 {
+        vec![]
+    } else {
+        let mut n: u64 = m;
+        let mut factor: u64 = 2;
+        let mut result: Vec<u64> = Vec::new();
+        loop {
+            if n % factor == 0 {
+                result.push(factor);
+                n = n / factor;
+            } else {
+                factor = factor + 1;
+            }
+            if n <= 1 {
+                break;
+            }
+        }
+        result
+    }
 }
