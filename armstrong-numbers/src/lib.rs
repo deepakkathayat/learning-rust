ccdf4c741ff562330b187966b4bc3325737fafd1
Deepak M. Kathayat <dmkathayat@gmail.com>
2019-04-27 18:43:51 +0530

    use to_string and to_digit

--- armstrong-numbers/src/lib.rs
+++ armstrong-numbers/src/lib.rs
@@ -1,10 +1,7 @@
 pub fn is_armstrong_number(num: u32) -> bool {
-    let mut n: u32 = num;
-    let mut digits: Vec<u32> = Vec::new();
-    while n > 0 {
-        digits.push(n % 10);
-        n = n / 10;
-    }
-    let len = digits.len() as u32;
-    num == digits.iter().fold(0, |acc, x| acc + x.pow(len))
+    let num_str = num.to_string();
+    let len = num_str.len() as u32;
+    num == num_str
+        .chars()
+        .fold(0, |acc, x| acc + x.to_digit(10).unwrap().pow(len))
 }
