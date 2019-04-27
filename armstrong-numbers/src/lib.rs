aa5e74805d6af14f62076a2bfe91a226152c8916
Deepak M. Kathayat <dmkathayat@gmail.com>
2019-04-27 18:37:46 +0530

    implement

--- armstrong-numbers/src/lib.rs
+++ armstrong-numbers/src/lib.rs
@@ -1,3 +1,10 @@
 pub fn is_armstrong_number(num: u32) -> bool {
-    unimplemented!("true if {} is an armstrong number", num)
+    let mut n: u32 = num;
+    let mut digits: Vec<u32> = Vec::new();
+    while n > 0 {
+        digits.push(n % 10);
+        n = n / 10;
+    }
+    let len = digits.len() as u32;
+    num == digits.iter().fold(0, |acc, x| acc + x.pow(len))
 }
