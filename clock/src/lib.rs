b985bcaa2c2fa2c2abcbf9d2bc8b2313840afff1
Deepak M. Kathayat <dmkathayat@gmail.com>
2019-04-29 10:05:49 +0530

    implement struct with debug g and partialeq

--- clock/src/lib.rs
+++ clock/src/lib.rs
@@ -1,15 +1,23 @@
-pub struct Clock;
+#[derive(Debug, PartialEq)]
+pub struct Clock {
+    hours: i32,
+    minutes: i32,
+}
 
 impl Clock {
     pub fn new(hours: i32, minutes: i32) -> Self {
-        unimplemented!(
-            "Construct a new Clock from {} hours and {} minutes",
-            hours,
-            minutes
-        );
+        let clock = Clock {
+            hours: hours,
+            minutes: minutes,
+        };
+        clock
     }
 
     pub fn add_minutes(&self, minutes: i32) -> Self {
         unimplemented!("Add {} minutes to existing Clock time", minutes);
+    }
+
+    pub fn to_string(&self) -> String {
+        format!("{:02}:{:02}", self.hours, self.minutes)
     }
 }
