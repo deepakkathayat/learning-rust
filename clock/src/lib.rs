ba765eb5a699140bbfab2624860e9e96bcf9e365
Deepak M. Kathayat <dmkathayat@gmail.com>
2019-04-30 22:58:40 +0530

    add minutes

--- clock/src/lib.rs
+++ clock/src/lib.rs
@@ -6,7 +6,7 @@ pub struct Clock {
 
 impl Clock {
     pub fn new(hours: i32, minutes: i32) -> Self {
-        let h = (hours as f32 + ((minutes / 60) as f32).floor()) as i32 % 24;
+        let h = (hours as f32 + (minutes as f32 / 60.0).floor()) as i32 % 24;
         let m = minutes % 60;
         let clock = Clock {
             hours: if h >= 0 { h } else { h + 24 },
@@ -15,8 +15,11 @@ impl Clock {
         clock
     }
 
-    pub fn add_minutes(&self, minutes: i32) -> Self {
-        unimplemented!("Add {} minutes to existing Clock time", minutes);
+    pub fn add_minutes(self, minutes: i32) -> Self {
+        self.minutes += minutes;
+        self.hours = (self.hours as f32 + (self.minutes as f32 / 60.0).floor()) as i32 % 24;
+        self.minutes %= 60;
+        self
     }
 
     pub fn to_string(&self) -> String {
