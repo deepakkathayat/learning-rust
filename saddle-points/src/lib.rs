a45483d2ad52b5787f58d78fd742cd499703c46b
Deepak M. Kathayat <dmkathayat@gmail.com>
2019-05-01 17:48:08 +0530

    use iters

--- saddle-points/src/lib.rs
+++ saddle-points/src/lib.rs
@@ -1,6 +1,12 @@
 pub fn find_saddle_points(input: &[Vec<u64>]) -> Vec<(usize, usize)> {
-    unimplemented!(
-        "find the saddle points of the following matrix: {:?}",
-        input
-    )
+    let mut result: Vec<(usize, usize)> = Vec::new();
+
+    for (r, row) in input.iter().enumerate() {
+        for (c, item) in row.iter().enumerate() {
+            if row.iter().all(|x| x <= item) && (0..input.len()).all(|x| input[x][c] >= *item) {
+                result.push((r, c));
+            }
+        }
+    }
+    result
 }
