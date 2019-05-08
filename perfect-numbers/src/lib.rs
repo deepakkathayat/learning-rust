29e02e1fdf1b7fbe68d0e1cb1efb3396824d8fde
Deepak M. Kathayat <dmkathayat@gmail.com>
2019-05-08 22:53:40 +0530

    fix match and handle 0

--- perfect-numbers/src/lib.rs
+++ perfect-numbers/src/lib.rs
@@ -6,11 +6,15 @@ pub enum Classification {
 }
 
 pub fn classify(num: u64) -> Option<Classification> {
-    let sum = (1..num).into_iter().filter(|&x| num % x == 0).sum();
+    if num == 0 {
+        return None;
+    }
+    let sum: u64 = (1..num).into_iter().filter(|&x| num % x == 0).sum();
 
     match sum {
-        sum if sum == num => Some(Perfect),
-        sum if sum < num => Some(Deficient),
-        sum if sum > num => Some(Abundant),
+        sum if sum == num => Some(Classification::Perfect),
+        sum if sum > num => Some(Classification::Abundant),
+        sum if sum < num => Some(Classification::Deficient),
+        _ => None,
     }
 }
