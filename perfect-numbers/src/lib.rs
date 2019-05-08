da8ee98208d739d7cd704ed33ccbc8059d347d82
Deepak M. Kathayat <dmkathayat@gmail.com>
2019-05-08 22:31:36 +0530

    fix iter

--- perfect-numbers/src/lib.rs
+++ perfect-numbers/src/lib.rs
@@ -6,11 +6,11 @@ pub enum Classification {
 }
 
 pub fn classify(num: u64) -> Option<Classification> {
-    let sum = (1..num).iter().filter(|&x| num % x == 0).sum();
+    let sum = (1..num).into_iter().filter(|&x| num % x == 0).sum();
 
     match sum {
-        sum if sum == num => Option(Perfect),
-        sum if sum < num => Option(Deficient),
-        sum if sum > num => Option(Deficient),
+        sum if sum == num => Some(Perfect),
+        sum if sum < num => Some(Deficient),
+        sum if sum > num => Some(Abundant),
     }
 }
