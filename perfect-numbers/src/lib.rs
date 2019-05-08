161c19ab4fdd9de5c8a19061a4959b969cff464f
Deepak M. Kathayat <dmkathayat@gmail.com>
2019-05-08 22:58:07 +0530

    use Ok and limit iteration

--- perfect-numbers/src/lib.rs
+++ perfect-numbers/src/lib.rs
@@ -9,12 +9,12 @@ pub fn classify(num: u64) -> Option<Classification> {
     if num == 0 {
         return None;
     }
-    let sum: u64 = (1..num).into_iter().filter(|&x| num % x == 0).sum();
+    let sum: u64 = (1..num / 2 + 1).into_iter().filter(|&x| num % x == 0).sum();
 
     match sum {
-        sum if sum == num => Some(Classification::Perfect),
-        sum if sum > num => Some(Classification::Abundant),
-        sum if sum < num => Some(Classification::Deficient),
+        sum if sum == num => Ok(Classification::Perfect),
+        sum if sum > num => Ok(Classification::Abundant),
+        sum if sum < num => Ok(Classification::Deficient),
         _ => None,
     }
 }
