5842e4aea7e5d8ac6828058d086728af1aabe50e
Deepak M. Kathayat <dmkathayat@gmail.com>
2019-05-08 22:29:13 +0530

    use filter and match

--- perfect-numbers/src/lib.rs
+++ perfect-numbers/src/lib.rs
@@ -6,5 +6,11 @@ pub enum Classification {
 }
 
 pub fn classify(num: u64) -> Option<Classification> {
-    unimplemented!("classify {}", num);
+    let sum = (1..num).iter().filter(|&x| num % x == 0).sum();
+
+    match sum {
+        sum if sum == num => Option(Perfect),
+        sum if sum < num => Option(Deficient),
+        sum if sum > num => Option(Deficient),
+    }
 }
