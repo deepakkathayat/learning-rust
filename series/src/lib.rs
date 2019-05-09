8ca97c45a061cf8b1e2d407a225835ce44bfcd68
Deepak M. Kathayat <dmkathayat@gmail.com>
2019-05-10 00:00:40 +0530

    use skip and take

--- series/src/lib.rs
+++ series/src/lib.rs
@@ -1,11 +1,5 @@
 pub fn series(digits: &str, len: usize) -> Vec<String> {
-    if len == 0 {
-        return vec!["".to_string(); digits.len() + 1];
-    }
-    digits
-        .chars()
-        .collect::<Vec<char>>()
-        .windows(len)
-        .map(|x| x.into_iter().collect())
+    (0..digits.len() + 1 - len)
+        .map(|i| digits.chars().skip(i).take(len).collect())
         .collect()
 }
