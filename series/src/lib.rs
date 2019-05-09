60f59dc078fe0d2dcebc8b5b16a7e9637fbc753f
Deepak M. Kathayat <dmkathayat@gmail.com>
2019-05-09 08:18:10 +0530

    implement using windows

--- series/src/lib.rs
+++ series/src/lib.rs
@@ -1,7 +1,14 @@
 pub fn series(digits: &str, len: usize) -> Vec<String> {
-    unimplemented!(
-        "What are the series of length {} in string {:?}",
-        len,
-        digits
-    )
+    if len > digits.len() {
+        return vec![];
+    }
+    if len == 0 {
+        return vec!["".to_string(); digits.len() + 1];
+    }
+    digits
+        .chars()
+        .collect::<Vec<char>>()
+        .windows(len)
+        .map(|x| x.into_iter().collect())
+        .collect()
 }
