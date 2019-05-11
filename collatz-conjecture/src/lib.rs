36d3b1b64d10a57547b697631533bc879727c587
Deepak M. Kathayat <deepak.mk17@gmail.com>
2019-05-11 22:50:28 +0530

    implement using while

--- collatz-conjecture/src/lib.rs
+++ collatz-conjecture/src/lib.rs
@@ -1,6 +1,17 @@
-pub fn collatz(n: u64) -> Option<u64> {
-    unimplemented!(
-        "return Some(x) where x is the number of steps required to reach 1 starting with {}",
-        n,
-    )
+pub fn collatz(mut n: u64) -> Option<u64> {
+    if n == 0 {
+        return None;
+    }
+    let mut steps: u64 = 0;
+
+    while n > 1 {
+        if n%2 == 0 {
+            n = n/2;
+        }
+        else {
+            n = 3*n+1;
+        }
+        steps += 1;
+    }
+    Some(steps)
 }
