fa97988fa4294cee43a70afa4cd59092417b5264
Deepak M. Kathayat <deepak.mk17@gmail.com>
2019-05-12 15:47:29 +0530

    clean up

--- diffie-hellman/src/lib.rs
+++ diffie-hellman/src/lib.rs
@@ -6,7 +6,7 @@ fn mod_pow(mut b: u64, mut e: u64, m: u64) -> u64 {
     if m == 1 {
         return 0;
     }
-    let mut result = 1;
+    let mut result: u64 = 1;
     b = b % m;
     while e > 0 {
         if e%2 == 1 {
@@ -19,8 +19,7 @@ fn mod_pow(mut b: u64, mut e: u64, m: u64) -> u64 {
 }
 
 pub fn private_key(p: u64) -> u64 {
-    let mut rng = rand::thread_rng();
-    rng.gen_range(2,p)
+    rand::thread_rng().gen_range(2u64,p)
 }
 
 pub fn public_key(p: u64, g: u64, a: u64) -> u64 {
