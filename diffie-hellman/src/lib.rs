f1d4fad8719baaafffd1bc1e0b232b6d81d72524
Deepak M. Kathayat <deepak.mk17@gmail.com>
2019-05-12 15:29:11 +0530

    modlar exponention by iteration

--- diffie-hellman/src/lib.rs
+++ diffie-hellman/src/lib.rs
@@ -1,15 +1,28 @@
 extern crate rand;
 use rand::Rng;
 
+fn mod_pow(b: u64, mut e: u64, m: u64) -> u64 {
+    
+    if m == 1 {
+        return 0;
+    }
+    let mut c: u64 = 1;
+    while e > 0 {
+        c = (c*b) % m;
+        e-=1;
+    }
+    c
+}
+
 pub fn private_key(p: u64) -> u64 {
     let mut rng = rand::thread_rng();
     rng.gen_range(2,p)
 }
 
 pub fn public_key(p: u64, g: u64, a: u64) -> u64 {
-    g.pow(a) % p
+    mod_pow(g, a, p)
 }
 
 pub fn secret(p: u64, b_pub: u64, a: u64) -> u64 {
-    b_pub.pow(a) % p
+    mod_pow(b_pub,a,p)
 }
