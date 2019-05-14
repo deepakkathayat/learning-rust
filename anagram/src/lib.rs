8ed3030d2388561cce929391d2478cf2303c87e9
Deepak M. Kathayat <deepak.mk17@gmail.com>
2019-05-14 23:44:21 +0530

    simplify collection

--- anagram/src/lib.rs
+++ anagram/src/lib.rs
@@ -3,6 +3,11 @@ use std::collections::HashSet;
 pub fn anagrams_for<'a>(word: &str, possible_anagrams: &[&str]) -> HashSet<&'a str> {
     let mut w = word.chars().collect::<Vec<char>>();
     w.sort();
-    let res = possible_anagrams.iter().filter(|s| {let mut st = s.chars().collect::<Vec<char>>(); st.sort(); st} == w).collect();
-    res
+    let res = possible_anagrams.iter().filter(|s| {let mut st = s.chars().collect::<Vec<char>>(); st.sort(); st} == w);
+
+    let result: HashSet<&'a str>  = HashSet::new();
+    for item in res {
+        println!("{}",item);
+    }
+    result
 }
