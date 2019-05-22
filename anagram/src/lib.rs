9d7cca716713d17dda38ae52eea626c00fb86431
Deepak M. Kathayat <dmkathayat@gmail.com>
2019-05-22 08:45:22 +0530

    fix comparison with self

--- anagram/src/lib.rs
+++ anagram/src/lib.rs
@@ -1,18 +1,18 @@
 use std::collections::HashSet;
 
 pub fn anagrams_for<'a>(word: &str, possible_anagrams: &[&'a str]) -> HashSet<&'a str> {
-    let mut w = word.to_ascii_lowercase().chars().collect::<Vec<char>>();
+    let mut w = word.to_lowercase().chars().collect::<Vec<char>>();
     w.sort();
     println!("{:?}", w);
     let res: HashSet<&'a str> = possible_anagrams
         .iter()
         .filter(
             |s| {
-                let mut st = s.to_ascii_lowercase().chars().collect::<Vec<char>>();
+                let mut st = s.to_lowercase().chars().collect::<Vec<char>>();
                 st.sort();
                 println!("{:?}", st);
                 st
-            } == w,
+            } == w && word.to_lowercase() != s.to_lowercase(),
         )
         .map(|s| *s)
         .collect();
