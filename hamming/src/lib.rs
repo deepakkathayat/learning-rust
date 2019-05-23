9110b9465f20d710911ccbd4cfd51ba15a96a30e
Deepak M. Kathayat <dmkathayat@gmail.com>
2019-05-23 20:45:57 +0530

    use pair

--- hamming/src/lib.rs
+++ hamming/src/lib.rs
@@ -4,9 +4,9 @@ pub fn hamming_distance(s1: &str, s2: &str) -> Option<usize> {
     if s1.len() != s2.len() {
         None
     } else {
-        Some(s1.chars().zip(s2.chars()).fold(0, |acc, x| {
-            acc + {
-                if x.0 != x.1 {
+        Some(s1.chars().zip(s2.chars()).fold(0, |dist, (a, b)| {
+            dist + {
+                if a != b {
                     1
                 } else {
                     0
