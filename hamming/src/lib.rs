b8a09a20ea023ce8043ce51c8bd80a20b3dc9d6d
Deepak M. Kathayat <dmkathayat@gmail.com>
2019-05-23 20:43:20 +0530

    clean up

--- hamming/src/lib.rs
+++ hamming/src/lib.rs
@@ -2,15 +2,16 @@
 /// or None if the lengths are mismatched.
 pub fn hamming_distance(s1: &str, s2: &str) -> Option<usize> {
     if s1.len() != s2.len() {
-        return None;
-    }
-    Some(s1.chars().zip(s2.chars()).fold(0, |acc, x| {
-        acc + {
-            if x.0 != x.1 {
-                1
-            } else {
-                0
+        None
+    } else {
+        Some(s1.chars().zip(s2.chars()).fold(0, |acc, x| {
+            acc + {
+                if x.0 != x.1 {
+                    1
+                } else {
+                    0
+                }
             }
-        }
-    }))
+        }))
+    }
 }
