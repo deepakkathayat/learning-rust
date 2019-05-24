c77517751dd446a74ce7872be7a2a94ae235baa3
Deepak M. Kathayat <dmkathayat@gmail.com>
2019-05-24 22:56:12 +0530

    use all with simple u8 range

--- pangram/src/lib.rs
+++ pangram/src/lib.rs
@@ -1,6 +1,4 @@
 /// Determine whether a sentence is a pangram.
 pub fn is_pangram(sentence: &str) -> bool {
-    "abcdefghijklmnopqrstuvwxyz"
-        .chars()
-        .all(|c| sentence.to_lowercase().contains(c))
+    (b'a'..b'z').all(|c| sentence.to_lowercase().contains(c as char))
 }
