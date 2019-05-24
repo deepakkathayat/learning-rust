6e57b562dee2b2a8346831e6570da9db1d6b224a
Deepak M. Kathayat <dmkathayat@gmail.com>
2019-05-24 22:24:00 +0530

    use all

--- pangram/src/lib.rs
+++ pangram/src/lib.rs
@@ -1,13 +1,6 @@
-use std::collections::HashSet;
-
 /// Determine whether a sentence is a pangram.
 pub fn is_pangram(sentence: &str) -> bool {
-    let pangram: HashSet<char> = "abcdefghijklmnopqrstuvwxyz".chars().collect();
-
-    let candidate = sentence
+    "abcdefghijklmnopqrstuvwxyz"
         .chars()
-        .filter(|&x| x.is_ascii_alphabetic())
-        .map(|x| x.to_ascii_lowercase())
-        .collect::<HashSet<char>>();
-    pangram == candidate
+        .all(|c| sentence.to_lowercase().contains(c))
 }
