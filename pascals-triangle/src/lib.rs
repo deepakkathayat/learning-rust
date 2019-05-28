86ad7e87b9955057dc16cbdc3295fb98d9f539f5
Deepak M. Kathayat <dmkathayat@gmail.com>
2019-05-28 20:59:23 +0530

    implement new and loop

--- pascals-triangle/src/lib.rs
+++ pascals-triangle/src/lib.rs
@@ -1,11 +1,27 @@
-pub struct PascalsTriangle;
+pub struct PascalsTriangle {
+    row_count: u32,
+    rows: Vec<Vec<u32>>,
+}
 
 impl PascalsTriangle {
     pub fn new(row_count: u32) -> Self {
-        unimplemented!("create Pascal's triangle with {} rows", row_count);
+        let mut P = PascalsTriangle {
+            row_count: row_count,
+            rows: vec![vec![]],
+        };
+        P.rows = P.rows();
+        P
     }
 
     pub fn rows(&self) -> Vec<Vec<u32>> {
-        unimplemented!();
+        let mut r: u32 = self.row_count;
+        let mut v: Vec<Vec<u32>> = Vec::new();
+        while r > 0 {
+            let row_size = self.row_count - r + 1;
+            let mut row: Vec<u32> = Vec::new();
+
+            r -= 1;
+        }
+        v
     }
 }
