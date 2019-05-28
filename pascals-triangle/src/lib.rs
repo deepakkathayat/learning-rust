9d3d3224657bf549bc0a71161d44a0b83863ee1a
Deepak M. Kathayat <dmkathayat@gmail.com>
2019-05-28 21:08:57 +0530

    compute pascal triangle

--- pascals-triangle/src/lib.rs
+++ pascals-triangle/src/lib.rs
@@ -5,21 +5,29 @@ pub struct PascalsTriangle {
 
 impl PascalsTriangle {
     pub fn new(row_count: u32) -> Self {
-        let mut P = PascalsTriangle {
+        let mut p_triangle = PascalsTriangle {
             row_count: row_count,
             rows: vec![vec![]],
         };
-        P.rows = P.rows();
-        P
+        p_triangle.rows = p_triangle.rows();
+        p_triangle
     }
 
     pub fn rows(&self) -> Vec<Vec<u32>> {
         let mut r: u32 = self.row_count;
         let mut v: Vec<Vec<u32>> = Vec::new();
         while r > 0 {
-            let row_size = self.row_count - r + 1;
+            let row_size: usize = (self.row_count - r + 1) as usize;
             let mut row: Vec<u32> = Vec::new();
 
+            for i in 0..row_size {
+                if i == 0 || i == row_size - 1 {
+                    row.push(1);
+                } else {
+                    row.push(v[row_size - 2][i - 1] + v[row_size - 2][i]);
+                }
+            }
+            v.push(row);
             r -= 1;
         }
         v
