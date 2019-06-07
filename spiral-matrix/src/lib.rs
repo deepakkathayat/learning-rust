00b2c5dc78fa63df1fec7feddb895fa6af0f46f9
Deepak M. Kathayat <dmkathayat@gmail.com>
2019-06-07 23:08:57 +0530

    add direction enum

--- spiral-matrix/src/lib.rs
+++ spiral-matrix/src/lib.rs
@@ -1,15 +1,26 @@
+use DIRECTION::*;
+enum DIRECTION {
+    UP,
+    DOWN,
+    RIGHT,
+    LEFT,
+}
+
+fn advance(res: &Vec<Vec<u32>>, i: &usize, j: &usize, dir: &DIRECTION) {}
 pub fn spiral_matrix(size: u32) -> Vec<Vec<u32>> {
     if size == 0 {
         return vec![vec![]];
     }
     let mut res: Vec<Vec<u32>> = (0..size).map(|_| vec![0; size as usize]).collect();
 
-    let mut i = 0;
-    let mut j = 0;
-    let mut item = 1;
+    let mut i: usize = 0;
+    let mut j: usize = 0;
+    let mut item: u32 = 1;
+    let mut dir: DIRECTION = RIGHT;
     loop {
         res[i][j] = item;
-        advance(res, i, j);
+        advance(&res, &i, &j, &dir);
+        item += 1;
     }
     res
 }
