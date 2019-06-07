1a71cc81cfd1eab8686a787474df2751db519e56
Deepak M. Kathayat <dmkathayat@gmail.com>
2019-06-07 23:01:41 +0530

    initialize empty vector

--- spiral-matrix/src/lib.rs
+++ spiral-matrix/src/lib.rs
@@ -1,6 +1,15 @@
 pub fn spiral_matrix(size: u32) -> Vec<Vec<u32>> {
-    unimplemented!(
-        "Function that returns the spiral matrix of square size {}",
-        size
-    );
+    if size == 0 {
+        return vec![vec![]];
+    }
+    let mut res: Vec<Vec<u32>> = (0..size).map(|_| vec![0; size as usize]).collect();
+
+    let mut i = 0;
+    let mut j = 0;
+    let mut item = 1;
+    loop {
+        res[i][j] = item;
+        advance(res, i, j);
+    }
+    res
 }
