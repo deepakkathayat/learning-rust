3b98de4f6b967763855ab615698b8510407cbfda
Deepak M. Kathayat <dmkathayat@gmail.com>
2019-07-06 22:58:14 +0530

    return empty if empty phrase

--- acronym/src/lib.rs
+++ acronym/src/lib.rs
@@ -1,1 +1,8 @@
-pub fn abbreviate(phrase: &str) -> String {}
+pub fn abbreviate(phrase: &str) -> String {
+    let mut result = String::new();
+    if phrase.len() == 0 {
+        return result;
+    }
+
+    for (i, &ch) in phrase.iter().enumerate() {}
+}
