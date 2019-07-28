3ec472d04793810ad0a645d829af5ea59528cced
Deepak M. Kathayat <dmkathayat@gmail.com>
2019-07-28 20:59:36 +0530

    clean up

--- network-programming/Chapter03/my-tcp-echo-server.rs
+++ network-programming/Chapter03/my-tcp-echo-server.rs
@@ -1,23 +1,31 @@
 use std::io::{Error, Read, Write};
-use std::net::TcpListener;
+use std::net::{TcpListener, TcpStream};
+use std::thread;
+
+fn handle_connection(mut stream: TcpStream) {
+    let mut buffer = [0; 512];
+    stream.read(&mut buffer).unwrap();
+
+    println!(
+        "Connection established: {}",
+        String::from_utf8_lossy(&buffer[..])
+    );
+    let response = "HTTP/1.1 200 OK\r\n\r\n";
+
+    stream.write(response.as_bytes()).unwrap();
+    stream.flush().unwrap();
+}
+
 fn main() {
     let listener = TcpListener::bind("0.0.0.0:8888").unwrap();
 
     for stream in listener.incoming() {
         match stream {
             Err(e) => eprintln!("failed: {}", e),
-            Ok(mut stream) => {
-                let mut buffer = [0; 512];
-                stream.read(&mut buffer).unwrap();
-
-                println!(
-                    "Connection established: {}",
-                    String::from_utf8_lossy(&buffer[..])
-                );
-                let response = "HTTP/1.1 200 OK\r\n\r\n";
-
-                stream.write(response.as_bytes()).unwrap();
-                stream.flush().unwrap();
+            Ok(stream) => {
+                thread::spawn(|| {
+                    handle_connection(stream);
+                });
             }
         }
     }
