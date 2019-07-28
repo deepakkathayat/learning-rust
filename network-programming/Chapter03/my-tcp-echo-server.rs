694010042ab1f30c8c4f0fa4e5c0424eae649826
Deepak M. Kathayat <dmkathayat@gmail.com>
2019-07-28 17:09:02 +0530

    read stream

--- network-programming/Chapter03/my-tcp-echo-server.rs
+++ network-programming/Chapter03/my-tcp-echo-server.rs
@@ -1,13 +1,19 @@
+use std::io::{Error, Read, Write};
 use std::net::TcpListener;
-
 fn main() {
     let listener = TcpListener::bind("0.0.0.0:8888").unwrap();
 
     for stream in listener.incoming() {
         match stream {
             Err(e) => eprintln!("failed: {}", e),
-            Ok(stream) => {
-                println!("Connection established!");
+            Ok(mut stream) => {
+                let mut buffer = [0; 512];
+                stream.read(&mut buffer).unwrap();
+
+                println!(
+                    "Connection established: {}",
+                    String::from_utf8_lossy(&buffer[..])
+                );
             }
         }
     }
